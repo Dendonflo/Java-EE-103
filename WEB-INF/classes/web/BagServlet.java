16ac89b7d15154e0f00ca81360f6592857e2259a
DENODNCKER Florian <dendonflo@gmail.com>
2021-09-20 16:55:29 +0200

    Correction partie 1

--- WEB-INF/classes/web/BagServlet.java
+++ WEB-INF/classes/web/BagServlet.java
@@ -15,7 +15,7 @@ public class BagServlet extends HttpServlet{
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) 
 	throws ServletException, IOException {
-		res.setContentType("text/HTML");
+		res.setContentType("text/html");
 		PrintWriter out = res.getWriter();
 		out.println("<form method=\"POST\" action=\"bag\">");
 		out.println("<input type=\"text\" name=\"ref\" value=\"Reference\">");
@@ -28,14 +28,20 @@ public class BagServlet extends HttpServlet{
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) 
 	throws ServletException, IOException {
-		res.setContentType("text/HTML");
+		res.setContentType("text/html");
 		PrintWriter out = res.getWriter();
 		String ref = req.getParameter("ref");
 		String qty = req.getParameter("qty");
-		if((ref != null) and (qty != null)){
-			out.println(ref);
-			out.println(qty);
+		if((ref == "") || (qty == "") || (ref == null) || (qty == null)){
+			//res.setStatus(400);
+			res.sendError(400,"At least one paramater is empty !");
 		}
+		else{if(!(qty.matches("[0-9]+"))){
+			res.sendError(400,"Quantity MUST be a number !");
+		}}
+		out.println(ref);
+		out.println(qty);
+
 	}
 	
 	
