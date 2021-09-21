1ff7aa7c9226354aa30ecee9034ac39e9ba574df
DENODNCKER Florian <dendonflo@gmail.com>
2021-09-21 11:12:32 +0200

    partie sessiosn terminée

--- WEB-INF/classes/web/BagServlet.java
+++ WEB-INF/classes/web/BagServlet.java
@@ -10,14 +10,19 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.WebServlet;
 @WebServlet(urlPatterns={"/bag"})
 public class BagServlet extends HttpServlet{
-	
-	Bag myBag = new Bag();
+
+	//Bag myBag = null;
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) 
 	throws ServletException, IOException {
+
+		HttpSession session = req.getSession(true);
+		Bag myBag = (Bag)session.getAttribute("bag");
+
 		res.setContentType("text/html");
 		PrintWriter out = res.getWriter();
-		myBag.print(out);
+
+		if (myBag != null) myBag.print(out);
 		out.println("<form method=\"POST\" action=\"bag\">");
 		out.println("<input type=\"text\" name=\"ref\" value=\"Reference\">");
 		out.println("<input type=\"text\" name=\"qty\" value=\"Quantite\">");
@@ -29,10 +34,17 @@ public class BagServlet extends HttpServlet{
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) 
 	throws ServletException, IOException {
+		HttpSession session = req.getSession(true);
+		if (session.getAttribute("bag") == null){
+			session.setAttribute("bag",new Bag());
+		}
+
 		res.setContentType("text/html");
 		PrintWriter out = res.getWriter();
 		String ref = req.getParameter("ref");
 		String qty = req.getParameter("qty");
+
+		// VERIFICATION DONNEES ENTREES PAR L'UTILISATEUR
 		if((ref == "") || (qty == "") || (ref == null) || (qty == null)){
 			//res.setStatus(400);
 			res.sendError(400,"At least one paramater is empty !");
@@ -40,7 +52,11 @@ public class BagServlet extends HttpServlet{
 		else{if(!(qty.matches("[0-9]+"))){
 			res.sendError(400,"Quantity MUST be a number !");
 		}}
+
+
+		Bag myBag = (Bag)session.getAttribute("bag");
 		myBag.setItem(ref,Integer.parseInt(qty));
+		session.setAttribute("bag",myBag);
 		res.sendRedirect(req.getContextPath()+"/bag");
 
 	}
